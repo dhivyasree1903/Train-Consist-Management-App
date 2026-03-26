8a2f3808ddba90e0c1ba7321fcbc0f90d85a35f4
dhivyasree1903 <dhivyasree1903@gmail.com>
2026-03-26 09:52:21 +0530

    UC5: Preserve insertion order using LinkedHashSet

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.LinkedList;
+import java.util.LinkedHashSet;
+import java.util.Set;
 public class TrainConsistManagementApp {  public static void main(String[] args) {
     System.out.println("=== Train Consist Management App ===");
 
@@ -21,20 +23,17 @@ public class TrainConsistManagementApp {  public static void main(String[] args)
 
     if (passengerBogies.contains("Sleeper")) {
         System.out.println("\nSleeper bogie exists in train");
-    } else {
-        System.out.println("\nSleeper bogie not found");
     }
 
     System.out.println("\nFinal Passenger Bogies:");
     System.out.println(passengerBogies);
 
 
-    // UC4 - Maintain Ordered Train Consist
+    // UC4 - LinkedList
     System.out.println("\n=== Maintain Ordered Train Consist ===");
 
     LinkedList<String> trainConsist = new LinkedList<>();
 
-    // Add bogies
     trainConsist.add("Engine");
     trainConsist.add("Sleeper");
     trainConsist.add("AC");
@@ -44,18 +43,34 @@ public class TrainConsistManagementApp {  public static void main(String[] args)
     System.out.println("\nInitial Train Consist:");
     System.out.println(trainConsist);
 
-    // Insert Pantry Car at position 2
     trainConsist.add(2, "Pantry Car");
 
     System.out.println("\nAfter Adding Pantry Car:");
     System.out.println(trainConsist);
 
-    // Remove first and last bogie
     trainConsist.removeFirst();
     trainConsist.removeLast();
 
     System.out.println("\nFinal Ordered Train Consist:");
     System.out.println(trainConsist);
+
+
+    // UC5 - LinkedHashSet
+    System.out.println("\n=== Preserve Insertion Order of Bogies ===");
+
+    Set<String> formation = new LinkedHashSet<>();
+
+    // Add bogies
+    formation.add("Engine");
+    formation.add("Sleeper");
+    formation.add("Cargo");
+    formation.add("Guard");
+
+    // Add duplicate
+    formation.add("Sleeper");
+
+    System.out.println("\nFinal Train Formation:");
+    System.out.println(formation);
 
     System.out.println("\nProgram continues...");
 }
