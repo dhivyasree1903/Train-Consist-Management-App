daf44690babcbc1e5020930ceca7f8e5c65bd28e
dhivyasree1903 <dhivyasree1903@gmail.com>
2026-03-26 09:15:45 +0530

    UC1: Add welcome message module

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,2 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
 public class TrainConsistManagementApp {
+    public static void main(String[] args) {
+
+    System.out.println("=== Train Consist Management App ===");
+
+    List<String> trainConsist = new ArrayList<>();
+
+    System.out.println("Train consist initialized successfully.");
+    System.out.println("Initial Bogie Count: " + trainConsist.size());
+
+    System.out.println("Program continues...");
+}
+
 }
